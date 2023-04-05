96316fd51f9c010db7640e940d5c62d9947d9156
Shivani Choudhary <choudhary.shivani489@gmail.com>
2023-04-05 11:18:56 +0300

    chatproject

---
+++ src/pages/Register.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+export const Register = () => {
+    return (
+      
+      <div className= "Container">
+           <div className= "formContainer">
+      <span className="logo">Chit Chat</span>
+      <span className="title">Register</span>
+          <form>
+              <input type = "text" placeholder= "name" />
+              <input type="email" placeholder= "email"/>
+              <input type="password" placeholder="password"/>
+              <input type="file" />
+              <button>Sign Up</button>
+  
+          </form>
+          <p>You do not have an account? Login</p>
+          </div>
+          </div>
+          
+    )
+  }
+  
+
+
+export default Register
